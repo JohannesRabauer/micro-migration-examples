7450438874df88a1d03325e3d636a97f82d8644a
Johannes Rabauer <johannes@johannes-rabauer.de>
2020-12-13 18:55:31 +0100

    First working example

--- src/main/java/de/johannes_rabauer/micromigration/examples/simple/Main.java
+++ src/main/java/de/johannes_rabauer/micromigration/examples/simple/Main.java
@@ -4,14 +4,24 @@ import java.util.Date;
 
 import de.johannes_rabauer.micromigration.MigrationEmbeddedStorage;
 import de.johannes_rabauer.micromigration.MigrationEmbeddedStorageManager;
+import de.johannes_rabauer.micromigration.examples.simple.scripts.UpdateToV1_0;
+import de.johannes_rabauer.micromigration.examples.simple.scripts.UpdateToV1_1;
+import de.johannes_rabauer.micromigration.migrater.ExplicitMigrater;
 
 public class Main 
 {
 	public static void main(String[] args) 
 	{
-		final MigrationEmbeddedStorageManager storageManager = MigrationEmbeddedStorage.start();
+		final ExplicitMigrater migrater = new ExplicitMigrater(
+				new UpdateToV1_0(),
+				new UpdateToV1_1()
+		);
+		final MigrationEmbeddedStorageManager storageManager = MigrationEmbeddedStorage.start(migrater);
 		System.out.println(storageManager.root());
-		storageManager.setRoot("Hello World! @ " + new Date());
+		if(storageManager.root() == null)
+		{
+			storageManager.setRoot("Hello World! @ " + new Date());
+		}
 		storageManager.storeRoot();
 		storageManager.shutdown();
 	}
---
+++ src/main/java/de/johannes_rabauer/micromigration/examples/simple/scripts/UpdateToV1_0.java
@@ -0,0 +1,24 @@
+package de.johannes_rabauer.micromigration.examples.simple.scripts;
+
+import java.util.Date;
+
+import de.johannes_rabauer.micromigration.MicroMigrationScript;
+import de.johannes_rabauer.micromigration.MigrationEmbeddedStorageManager;
+import de.johannes_rabauer.micromigration.version.MicroMigrationVersion;
+
+public class UpdateToV1_0 implements MicroMigrationScript
+{
+	public MicroMigrationVersion getTargetVersion() 
+	{
+		return new MicroMigrationVersion(1,0);
+	}
+
+	public void execute(
+		Object                          root          ,
+		MigrationEmbeddedStorageManager storageManager
+	)
+	{
+		System.out.println("Update " + getTargetVersion().toString() + " executed.");
+		storageManager.setRoot("Hello World! @ " + new Date() + " Update 1.0");
+	}
+}
